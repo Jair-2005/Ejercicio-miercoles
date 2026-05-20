962276461c46d1a0dea901bd502675fb9328684a
Jair Granizo <steven.granizo@udla.edu.ec>
2026-05-20 14:42:46 -0500

    Segundo commit - promedio de notas

--- Ejercicio-en-clase.c
+++ Ejercicio-en-clase.c
@@ -4,9 +4,14 @@ int main (int argc, char *argv[]) {
 
     
     float notas[5]= {6.5, 7.1, 8, 6, 5};
+    float suma = 0.0f; 
 
     for (int i = 0; i < 5; i++) {
         printf("Notas [%d]: %.2f\n", i+1, notas[i]);
+        suma += notas[i];
     }   
+
+    float promedio = suma / 5.0f;
+    printf("Promedio: %.2f\n", promedio);
     return 0;
 }
