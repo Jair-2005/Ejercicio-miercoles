5f0d86b4d62b08a45568b64c4fe72393fcc5372e
Jair Granizo <steven.granizo@udla.edu.ec>
2026-05-20 15:03:21 -0500

    Tercer commit Funcion calcularPromedio x Valor

--- Ejercicio-en-clase.c
+++ Ejercicio-en-clase.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
+float calcularPromedio(float notas[5]);
+
 int main (int argc, char *argv[]) {
-
-    
+   
     float notas[5]= {6.5, 7.1, 8, 6, 5};
-    float suma = 0.0f; 
+    printf("Promedio: %.2f\n", calcularPromedio(notas));
+    return 0;
+}
 
+float calcularPromedio(float notas[5]) {
+    float suma = 0.0f;     
     for (int i = 0; i < 5; i++) {
         printf("Notas [%d]: %.2f\n", i+1, notas[i]);
         suma += notas[i];
     }   
 
     float promedio = suma / 5.0f;
-    printf("Promedio: %.2f\n", promedio);
-    return 0;
+    return promedio;
 }
